bde1f829cf690b49519087d7b7814aa3f0924f04
马卿 <admin@maiqngdemac-mini.local>
2016-11-07 19:08:01 +0800

    导入第三方和资源

---
+++ Emoji-System-Custom/Emoji-System-Custom/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Emoji-System-Custom
+//
+//  Created by 马卿 on 16/11/7.
+//  Copyright © 2016年 com.yihe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Emoji-System-Custom/Emoji-System-Custom/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Emoji-System-Custom
+//
+//  Created by 马卿 on 16/11/7.
+//  Copyright © 2016年 com.yihe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
